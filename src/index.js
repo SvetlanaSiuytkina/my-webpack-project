8c376689495dfd31d1a73d3802bb92948b7c7528
SVETA <svetadan1234@mail.ru>
2026-05-04 23:03:04 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+import moment from 'moment';
+import './script';
+
+console.log('App is running');
+console.log(moment.now());
---
+++ src/script.js
@@ -0,0 +1,4 @@
+import moment from 'moment';
+
+console.log('App is running');
+console.log(moment.now());
